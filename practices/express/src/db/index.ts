919681fc14bf6265e685b36b97bb4c6a080dab8c
mdasad6 <mdasaduzzamanasadfacebook@gmail.com>
2026-06-21 11:44:18 +0600

    feat: add profiles table creation in database initialization

--- practices/express/src/db/index.ts
+++ practices/express/src/db/index.ts
@@ -17,11 +17,24 @@ export const initDB = async () => {
       is_active BOOLEAN DEFAULT true,
       age INT,
 
-      created_at TIMESTAMP DEFAULT NOW() ,
+      created_at TIMESTAMP DEFAULT NOW(),
       updated_at TIMESTAMP DEFAULT NOW()
       )
 
       `);
+
+    await pool.query(`CREATE TABLE IF NOT EXISTS profiles(
+        id SERIAL PRIMARY KEY ,
+        user_id INT UNIQUE REFERENCES users(id) ON DELETE CASCADE,
+        bio TEXT,
+        address TEXT,
+        phone VARCHAR(20),
+        gender VARCHAR(10),
+
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+        )`);
+
     console.log("Database Connected Successfully!");
   } catch (error) {
     console.log(error);
